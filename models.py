a3415d0dcf7f6f7bfdf920ed44b839a5a3bc8e12
quantummole <35193295+quantummole@users.noreply.github.com>
2018-09-15 07:50:11 +0530

    adding a custom model for coarse segmentation and classification

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ Created on Mon Sep 10 12:14:19 2018
 import torch
 import torch.nn as nn
 import torch.nn.functional as tfunc
+from torchvision import models
 from model_blocks import DoubleConvLayer
 
 class create_net :
@@ -65,3 +66,46 @@ class CustomNet2(nn.Module):
             output = self.output_layer(inp)
             outputs.append(output)
         return outputs
+
+class CustomNet3(nn.Module):
+    def __init__(self,input_dim,initial_channels,growth_factor,num_classes_seg,num_classes_classify,hidden_size) :
+        super(CustomNet3,self).__init__()
+        self.layer = nn.ModuleList()
+        self.num_classes = num_classes_seg
+        self.num_classes_classifier = num_classes_classify
+        self.hidden_size = hidden_size
+
+        while input_dim >= 8 :
+            self.layer.append(nn.Sequential(DoubleConvLayer(initial_channels,initial_channels+growth_factor,3,1),
+                                            nn.MaxPool2d(kernel_size=3,stride=2,padding=1)))
+            input_dim = input_dim//2
+            initial_channels += growth_factor
+        self.final_size = input_dim*input_dim*initial_channels
+        self.output_layer = nn.Linear(self.final_size,self.num_classes)
+        self.rnn = nn.GRU(self.final_size,self.hidden_size,2,bidirectional=True)
+        self.output_classifier = nn.Linear(2*self.hidden_size,self.num_classes_classifier)
+        
+    def forward(self,inputs,mode=-1,debug=False) :
+        outputs = []
+        inputs = inputs[0]
+        if len(inputs.shape) == 3 :
+            bs,m,n = inputs.shape
+            inputs = inputs.view(bs,1,m,n)
+        bs,c,m,n = inputs.shape
+        init_hiddden = torch.zeros(4,bs,self.hidden_size).type_as(inputs)
+        embeddings = []
+        for i in range(0,m,32) :
+            for j in range(0,n,32) :
+                inp = inputs[:,:,i:i+32,j:j+32]
+                for layer in self.layer :
+                    inp = layer(inp)
+                inp = inp.view(bs,-1)
+                embeddings.append(inp)
+                output = self.output_layer(inp)
+                outputs.append(output)
+        outputs = torch.stack(outputs,dim=2).view(bs,self.num_classes,m//32,n//32)
+        rnn_inp = torch.stack(embeddings,dim=0)
+        rnn_output,_ = self.rnn(rnn_inp,init_hiddden)
+        rnn_output =  torch.mean(rnn_output,dim=0)
+        classification_output = self.output_classifier(rnn_output)
+        return [outputs,classification_output]
