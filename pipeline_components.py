f3c68cbb1cb8208c078084fa8e43fd88b2afbc58
quantummole <35193295+quantummole@users.noreply.github.com>
2018-09-23 11:35:12 +0530

    pretty printing at op initialization

--- pipeline_components.py
+++ pipeline_components.py
@@ -38,9 +38,11 @@ class Pipeline :
             scores.append(op_scores)
         return self.score_combiner(scores)
 
+
 class PipelineOp :
     def __init__(self,op_name,op_class,config_sets,evaluator = None) :
         self.op_name,self.op_class,self.config_sets,self.evaluator = op_name,op_class,config_sets,evaluator
+        print("{} has {} configurations".format(self.op_name,self.config_sets.max_configs))
         self.op_completion_state = Signal.COMPLETE
         self.completion_state = Signal.INCOMPLETE
         self.params = None
@@ -56,9 +58,10 @@ class PipelineOp :
         self.params_id = None
         self.curr_op = None
     def execute(self,score,global_params,inputs) :
-        if self.config_sets.completion_state == Signal.INCOMPLETE and self.op_completion_state == Signal.COMPLETE:
+        if self.completion_state == Signal.INCOMPLETE and self.op_completion_state == Signal.COMPLETE:
             self.params_id,self.params =  self.config_sets.get_next(score)
-            self.new_params = {**global_params,**self.params}
+            self.new_params = global_params
+            self.new_params[self.op_name] = self.params
             if self.evaluator :
                 self.evaluator.set_config(self.new_params)
                 self.evaluator.log_config()
