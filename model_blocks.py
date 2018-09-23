0570a9c88637e67e0d1106508316ac963c8a9242
quantummole <35193295+quantummole@users.noreply.github.com>
2018-09-23 11:33:39 +0530

    Inception layer to use double conv layers for various kernel sizes

--- model_blocks.py
+++ model_blocks.py
@@ -24,15 +24,11 @@ class DoubleConvLayer(nn.Module) :
 class InceptionLayer(nn.Module) :
     def __init__(self,in_channels,out_channels,kernel_sizes = [1,3,5]) :
         super(InceptionLayer,self).__init__()
-        intermed_channels = out_channels
+        intermed_channels = 2*out_channels
         self.layers = nn.ModuleList()
         for ksize in kernel_sizes :
-            self.layers.append(nn.Sequential(nn.Conv2d(in_channels,intermed_channels,kernel_size = ksize,padding=ksize//2),
-                                            nn.BatchNorm2d(intermed_channels),
-                                            nn.ReLU(inplace=True)))
-        self.output_layer = nn.Sequential(nn.Conv2d(len(kernel_sizes)*intermed_channels,out_channels,kernel_size=1),
-                                          nn.BatchNorm2d(out_channels),
-                                          nn.ReLU(inplace=True))
+            self.layers.append(DoubleConvLayer(in_channels,intermed_channels,ksize))
+        self.output_layer = nn.Sequential(DoubleConvLayer(len(kernel_sizes)*intermed_channels,out_channels,1))
     def forward(self,x) :
         outputs = []
         for layer in self.layers :
