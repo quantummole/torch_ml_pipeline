a250be1827e4164d97067c4f430408f70d0639d1
quantummole <35193295+quantummole@users.noreply.github.com>
2018-10-17 23:50:43 +0530

    fixing bug in stratifiedfolds

--- fold_strategy.py
+++ fold_strategy.py
@@ -52,6 +52,8 @@ class DeterministicFold(Fold) :
         curr_dataset = self.dataset
         for i in range(0,self.dataset.shape[0],self.validation_samples) :
             pruned_dataset = curr_dataset.drop(curr_indices)
+            if pruned_dataset.shape[0] == 0 :
+                break
             self.folds.append(pruned_dataset.sample(n=min([pruned_dataset.shape[0],self.validation_samples])))
             curr_indices = self.folds[-1].index
             curr_dataset = pruned_dataset
@@ -70,12 +72,17 @@ class StratifiedDeterministicFold(Fold) :
         self.folds = []
         curr_indices = []
         curr_dataset = self.dataset
+        dataset_groups = curr_dataset.groupby(self.group_keys,as_index=False,group_keys=False)
+        dataset_group_names = dataset_groups.groups.keys()
+        dataset_group_lengths = [len(dataset_groups.get_group(key)) for key in dataset_group_names]
+        sample_group_lengths = [np.int(np.ceil(x*self.validation_split)) for x in dataset_group_lengths]
         for i in range(0,self.dataset.shape[0],self.validation_samples) :
             pruned_dataset = curr_dataset.drop(curr_indices)
+            if pruned_dataset.shape[0] == 0 :
+                break
             dataset_groups = pruned_dataset.groupby(self.group_keys,as_index=False,group_keys=False)
             dataset_group_names = dataset_groups.groups.keys()
             dataset_group_lengths = [len(dataset_groups.get_group(key)) for key in dataset_group_names]
-            sample_group_lengths = [np.int(np.ceil(x*self.validation_split)) for x in dataset_group_lengths]
             fold = []
             for i,key in enumerate(dataset_group_names) :
                 fold.append(dataset_groups.get_group(key).sample(n=min([dataset_group_lengths[i],sample_group_lengths[i]])))
