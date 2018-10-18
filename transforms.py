c772cfca8c41b88c4f4ed5bcef1fbd9f4a7870c8
quantummole <35193295+quantummole@users.noreply.github.com>
2018-10-18 08:48:06 +0530

    adding a transform that is compatible with torchvision transforms

--- transforms.py
+++ transforms.py
@@ -6,7 +6,8 @@ Created on Tue Oct 16 19:41:52 2018
 """
 import random
 import numpy as np
-
+from PIL import Image
+from skimage import img_as_ubyte
 class BoolTransform :
     def __init__(self,transform,prob = 0.5) :
         self.transform = transform
@@ -19,6 +20,19 @@ class BoolTransform :
             if random.random() < self.prob :
                 return self.transform(x)
         return x
+
+class PILTransform :
+    def __init__(self,transform) :
+        self.transform = transform
+    def __call__(self,x) :
+        if isinstance(x,list) :
+            x = [np.array(self.transform(Image.fromarray(img_as_ubyte(y)))) for y in x]
+            return x
+        else :
+            x = Image.fromarray(img_as_ubyte(x))
+            x = self.transform(x)
+            x = np.array(x)
+            return x
 
 def channel_first(im) :
     im = np.transpose(im,(2,0,1))
